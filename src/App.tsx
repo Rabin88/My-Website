171778fb47a3bea48a5a22eee9024de65ead299d
Pun, Rabin <rabin.pun@keyloop.com>
2022-11-19 14:22:12 +0000

    added Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 // Components
+import { Box } from "@mui/material";
 import { Home } from "./Home";
+import { NavBar } from "./NavBar";
+// import { About } from "./About";
+// import { Portfolio } from "./Portfolio";
+// import { Contact } from "./Contact";
+// import { Copyright } from "./Copyright";
 
 const App = () => {
-  return <Home />;
+  return (
+    <Box>
+      <NavBar />
+      <Home />
+      {/* <About />
+      <Portfolio />
+      <Contact />
+      <Copyright /> */}
+    </Box>
+  );
 };
 
 export { App };
